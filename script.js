6bb5cefec01c9e2fa7a7840a8d6edd3906c34ac0
rardcaraig06 <95902479+rardcaraig06@users.noreply.github.com>
2022-03-26 21:29:30 +0800

    jvvb

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = window.prompt("What is your name?");
+var section = window.prompt("What is your section?");
+var grade = window.prompt("What is your target grade for this semester?");
+
+document.write ("mabuhay! " + name + ", you are looking great. welcome to " + section + " the best section ever. I promise to to do my best just to get " + grade + " for this semester.");
